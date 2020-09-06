b94ce20d173878f03a0fdbecd6f4ec7f8f0e7d72
Nicholas Apanian <nicholas.apanian@outlook.com>
2020-09-05 22:50:27 -0400

    initial setup

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          Welcome
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
